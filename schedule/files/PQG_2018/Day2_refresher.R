547fa37c143b885eff1627262b726ee3a9581690
rkhetani <khetani.r@gmail.com>
2018-07-20 19:14:38 -0400

    Create Day2_refresher.R

---
+++ schedule/files/PQG_2018/Day2_refresher.R
@@ -0,0 +1,31 @@
+# Refresher Exercise
+
+# 0. create the data frame called animals
+animals <- read.csv("data/animals.csv")
+
+# 1. Extract the speed of 40 km/h.
+animals$speed[1]
+animals$speed[animals$speed == 40]
+animals[1,1]
+animals[1,"speed"]
+animals["Elephant", "speed"]
+
+# 2. Return the rows with color of Tan.
+animals[c(2,5), ]
+animals[which(animals$color == "Tan"), ]
+animals[animals$color == "Tan",]
+
+# 3. Return the rows with speed greater than 50 km/h and output only the color column. Keep the output as a data frame.
+
+animals[animals$speed > 50, "color", drop =F]
+animals[animals$speed > 50, 2, drop=F]
+animals[which(animals$speed > 50), "color", drop =F]
+
+# 4. Change the color of “Grey” to “Gray”.
+
+animals$color != "Gray"
+animals[animals$color != "Gray",]
+
+animals[4,2] <- "Gray"
+animals[animals$color == "Grey", 2] <- "Gray"
+animals[animals$color == "Grey", "color"] <- "Gray"
